Fix weights calculation

experiments/chain_reps.py
@@ -1,33 +1,31 @@
 import logging
 
-import gym
 import torch
+import torch.nn.functional as F
 from bsuite.baselines.experiment import run
 from bsuite.utils import gym_wrapper
 from torch.utils.tensorboard import SummaryWriter
 
+from envs.nchain import NChainEnv
 from qreps.policy import DiscreteStochasticPolicy
 from qreps.reps import REPS
 
 for handler in logging.root.handlers[:]:
     logging.root.removeHandler(handler)
 
-
 FORMAT = "[%(asctime)s]: %(message)s"
 logging.basicConfig(level=logging.INFO, format=FORMAT)
 
-gym_env = gym.make("FrozenLake-v0")
-env = gym_wrapper.DMEnvFromGym(gym_env)
 
+gym_env = NChainEnv(n=10, slip=0, small=0.1, max_steps=200)
+env = gym_wrapper.DMEnvFromGym(gym_env)
 obs_num = env.observation_spec().num_values
 
-writer = SummaryWriter(comment="frozen_lake_reps")
+writer = SummaryWriter(comment="chain_reps")
 
 
 def feature_fn(x):
-    return torch.nn.functional.one_hot(torch.tensor(x, dtype=torch.int64), obs_num).to(
-        torch.float32
-    )
+    return F.one_hot(torch.tensor(x, dtype=torch.int64), obs_num).to(torch.float32)
 
 
 def pol_feature_fn(x):
@@ -38,17 +36,17 @@ policy = DiscreteStochasticPolicy(obs_num, env.action_spec().num_values)
 
 agent = REPS(
     feat_shape=(obs_num,),
-    sequence_length=200,
+    sequence_length=1000,
     writer=writer,
     val_feature_fn=feature_fn,
     pol_feature_fn=pol_feature_fn,
-    epsilon=0.8,
+    epsilon=1e-5,
     policy=policy,
     center_advantages=False,
 )
 
-
 run(agent, env, num_episodes=100)
+
 
 policy.set_eval_mode(True)
 
@@ -67,3 +65,8 @@ for i in range(5):
 
 logging.info(f"Val Reward {val_reward}")
 writer.add_scalar("val/reward", val_reward)
+
+for n in range(obs_num):
+    print(f"State {n}, Value: {agent.theta.dot(feature_fn(n))}")
+
+print("Policy", policy._policy)

qreps/policy.py
@@ -3,6 +3,7 @@ from typing import Union
 
 import numpy as np
 import torch
+import torch.nn.functional as F
 
 
 class Policy(ABC):
@@ -14,7 +15,7 @@ class Policy(ABC):
     @abstractmethod
     def fit(
         self, feat: torch.Tensor, actions: torch.Tensor, weights: torch.Tensor
-    ) -> torch.FloatTensor:
+    ) -> dict:
         """Fit the policy to the provided samples"""
         pass
 
@@ -23,6 +24,8 @@ class Policy(ABC):
 
 
 class DiscreteStochasticPolicy(Policy):
+    """Discrete Policy which assigns every action in every state a probability."""
+
     def __init__(self, n_states: int, n_actions: int):
         self._policy = torch.ones((n_states, n_actions))
         self._policy /= torch.sum(self._policy, 1, keepdim=True)
@@ -33,19 +36,28 @@ class DiscreteStochasticPolicy(Policy):
         if self._eval:
             return int(torch.argmax(self._policy[observation]).item())
 
-        m = torch.distributions.Categorical(self._policy[observation])
+        m = self._dist(observation)
         return int(m.sample())
 
-    def fit(self, feats, actions, weights):
-        states = feats
+    def _dist(self, observation):
+        return torch.distributions.Categorical(self._policy[observation])
 
-        for s, a, w in zip(states.long().numpy(), actions.long().numpy(), weights):
+    def fit(self, feats, actions, weights):
+        log_like_before = self._dist(feats).log_prob(actions)
+
+        for s, a, w in zip(feats.long().numpy(), actions.long().numpy(), weights):
             self._policy[s, a] = self._policy[s, a] * w
 
-        self._policy = torch.softmax(self._policy, 1)
+        self._policy = self._policy / torch.sum(self._policy, dim=1, keepdim=True)
 
+        log_like_after = self._dist(feats).log_prob(actions)
         # FIXME: Return real loss
-        return 0
+        return {
+            "policy_loss": 0,
+            "kl_loss": F.kl_div(
+                log_like_before, log_like_after, log_target=True, reduction="batchmean"
+            ),
+        }
 
     def set_eval_mode(self, enabled: bool):
         self._eval = enabled
@@ -73,14 +85,23 @@ class CategoricalMLP(Policy, torch.nn.Module):
             return torch.argmax(self.layer(observation))
 
     def fit(self, feat, actions, weights):
-
         loss = None
+        loglike_before = self._dist(feat).log_prob(actions)
+
         for epoch in range(self.minimizing_epochs):
             self.opt.zero_grad()
             loss = self.log_likelihood(actions, feat, weights)
             loss.backward()
             self.opt.step()
-        return loss
+
+        loglike_after = self._dist(feat).log_prob(actions)
+
+        return {
+            "policy_loss": loss,
+            "kl_loss": F.kl_div(
+                loglike_before, loglike_after, log_target=True, reduction="batchmean"
+            ),
+        }
 
     def log_likelihood(self, taken_actions, feat, weights) -> torch.FloatTensor:
         log_lik = self._dist(feat).log_prob(taken_actions)
@@ -97,13 +118,14 @@ class GaussianMLP(Policy, torch.nn.Module):
         act_shape,
         action_max,
         action_min,
+        sigma=1.0,
         minimizing_epochs=300,
         lr=1e-2,
     ):
         super(GaussianMLP, self).__init__()
         self.minimizing_epochs = minimizing_epochs
         self._mu = torch.nn.Linear(obs_shape, act_shape, bias=False)
-        self._sigma = torch.nn.Parameter(torch.ones(act_shape))
+        self._sigma = sigma
 
         self.opt = torch.optim.Adam(self.parameters(), lr=lr)
         self.stochastic = True
@@ -123,18 +145,25 @@ class GaussianMLP(Policy, torch.nn.Module):
             return torch.clamp(self._mu(observation), self.action_min, self.action_max)
 
     def fit(self, feat, actions, weights):
+        loglike_before = self.log_likelihood(feat, actions)
         loss = None
         self._mu.reset_parameters()
         for epoch in range(self.minimizing_epochs):
             self.opt.zero_grad()
-            loss = self.log_likelihood(actions, feat, weights)
+            loss = -torch.mean(weights * self.log_likelihood(feat, actions))
             loss.backward()
             self.opt.step()
-        return loss
 
-    def log_likelihood(self, taken_actions, feat, weights) -> torch.FloatTensor:
-        log_lik = self._dist(feat).log_prob(taken_actions)
-        return -torch.mean(weights * log_lik)
+        loglike_after = self.log_likelihood(feat, actions)
+        return {
+            "policy_loss": loss,
+            "kl_loss": F.kl_div(
+                loglike_before, loglike_after, log_target=True, reduction="batchmean"
+            ),
+        }
+
+    def log_likelihood(self, feat, taken_actions) -> torch.FloatTensor:
+        return self._dist(feat).log_prob(taken_actions)
 
     def set_eval_mode(self, enabled):
         self.stochastic = not enabled

qreps/reps.py
@@ -10,8 +10,8 @@ from bsuite.baselines.utils import sequence
 from torch import Tensor
 from torch.utils.tensorboard import SummaryWriter
 
+from .advantages import center_advantages
 from .buffer import Buffer
-from .feature_functions import bellman_error_batched
 from .policy import Policy
 from .reps_dual import reps_dual
 from .util import to_torch
@@ -33,6 +33,8 @@ class REPS(base.Agent):
         epsilon=1e-5,
         dual_optimizer_algorithm=nlopt.LD_SLSQP,
         writer: SummaryWriter = None,
+        center_advantages=True,
+        exp_limit=700,
     ):
         logger.info(f"Observations: {feat_shape}")
         logger.info(
@@ -45,6 +47,8 @@ class REPS(base.Agent):
         self.pol_feature_fn = pol_feature_fn
         self.theta = torch.rand(feat_shape)
         self.dual_optimizer_algorithm = dual_optimizer_algorithm
+        self.center_advantages = center_advantages
+        self.exp_limit = exp_limit
         self.policy = policy
         self.stochastic = True
         self.writer = writer
@@ -105,6 +109,14 @@ class REPS(base.Agent):
 
         return optimizer.last_optimum_value()
 
+    def calc_weights(self, features, rewards):
+        advantage = rewards - self.theta.matmul(features.T)
+        if self.center_advantages:
+            advantage = center_advantages(advantage)
+        return torch.exp(
+            torch.clamp(advantage / self.eta, -self.exp_limit, self.exp_limit)
+        )
+
     def update_policy(self, trajectory: sequence.Trajectory):
         observations, actions, rewards, discounts = trajectory
         features = self.feature_fn(observations[:-1])
@@ -116,22 +128,18 @@ class REPS(base.Agent):
         dual_loss = self.optimize_dual(features, features_next, discounted_rewards)
 
         # Calculate weights
-        weights = torch.exp(
-            bellman_error_batched(self.theta, features, features_next, rewards)
-            / self.eta
-        )
-        weights = torch.clamp(weights, 1e-75, np.inf)
-        weights = weights / torch.mean(weights, 0)
+        weights = self.calc_weights(features, rewards)
 
         pol_features = self.pol_feature_fn(observations[:-1])
         policy_loss = self.policy.fit(pol_features, actions, weights)
 
         logger.info(
-            f"Mean reward: {torch.mean(rewards):.2f}, Policy Loss: {policy_loss:.5f}, Dual Loss: {dual_loss:.2f}"
+            f"Mean reward: {torch.mean(rewards):.2f}, Dual Loss: {dual_loss:.2f}"
         )
         if self.writer is not None:
+            for key, value in policy_loss.items():
+                self.writer.add_scalar("train/" + key, value, self.iter)
             self.writer.add_scalar("train/mean_reward", torch.mean(rewards), self.iter)
-            self.writer.add_scalar("train/policy_loss", policy_loss, self.iter)
             self.writer.add_scalar("train/dual_loss", dual_loss, self.iter)
             self.writer.add_histogram("train/actions", actions, self.iter)
 